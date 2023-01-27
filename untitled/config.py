c4bd448af62d1511a763ad92c2e466673adacdf7
volcanic <78009909+Guncuke@users.noreply.github.com>
2023-01-28 00:21:24 +0800

    Update config.py

--- untitled/config.py
+++ untitled/config.py
@@ -1,12 +1,12 @@
-SECRET_KEY = 'alksjdlajlvjlas'
+SECRET_KEY = '给密码加密的密钥'
 
 
 # 数据库配置
 HOSTNAME = '127.0.0.1'
 PORT = 3306
 USERNAME = 'root'
-PASSWORD = 'wch010411'
-DATABASE = 'qa_platform'
+PASSWORD = 'xxxxxxxx'
+DATABASE = 'xxxxxxxx'
 DB_URI = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(USERNAME,PASSWORD,HOSTNAME,PORT,DATABASE)
 SQLALCHEMY_DATABASE_URI = DB_URI
 
@@ -14,6 +14,6 @@ SQLALCHEMY_DATABASE_URI = DB_URI
 MAIL_SERVER = 'smtp.qq.com'
 MAIL_USE_SSL = True
 MAIL_PORT = 465
-MAIL_USERNAME = '780530653@qq.com'
-MAIL_PASSWORD = 'kohvpkhruzdkbbaf'
-MAIL_DEFAULT_SENDER = '780530653@qq.com'
+MAIL_USERNAME = '服务器邮箱'
+MAIL_PASSWORD = '你自己的授权码'
+MAIL_DEFAULT_SENDER = '服务器邮箱'
